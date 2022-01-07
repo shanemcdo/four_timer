bc47c63999c6a8926883f650f5eb5bf34b1d64e4
KermitPurple <56333467+KermitPurple@users.noreply.github.com>
2022-01-07 20:15:00 +0000

    Deploying to gh-pages from @ KermitPurple/four_timer@58623bb603fc9a9fd212e06c011dd10f284c68b5 🚀

---
+++ js/index.js
@@ -0,0 +1,51 @@
+let timers = [new Timer(15), new Timer(15), new Timer(15), new Timer(15)];
+let timer_divs = document.querySelectorAll('.timer');
+let timer_elements = [];
+for(let i = 0; i < 4; i++){
+    timer_elements.push({
+        div: timer_divs[i],
+        time: timer_divs[i].querySelector('.time'),
+        total_time: timer_divs[i].querySelector('.total_time'),
+    });
+    timer_divs[i].addEventListener('click', event=>{
+        if(event.target.className == 'color' || event.target.className == 'flipper') return;
+        if(!timers[i].running){
+            timers[i].start();
+            timer_divs[i].style.borderColor = 'green';
+        }else{
+            timers[i].stop();
+            timer_divs[i].style.borderColor = '';
+        }
+        for(let j = 0; j < 4; j++){
+            if(j != i){
+                timers[j].stop();
+                timer_divs[j].style.borderColor = '';
+            }
+        }
+    });
+    let color = timer_divs[i].querySelector('.color');
+    color.addEventListener('input', ()=>{
+        set_background_color(i, color.value);
+    });
+    timer_divs[i].querySelector('.flipper').addEventListener('click', ()=>{
+        if(timer_divs[i].style.transform == '')
+            timer_divs[i].style.transform = 'rotate(180deg)';
+        else
+            timer_divs[i].style.transform = '';
+    });
+}
+
+setInterval(()=>{
+    for(let i = 0; i < 4; i++){
+        timer_elements[i].time.innerHTML = timers[i].date_string();
+        timer_elements[i].total_time.innerHTML = timers[i].total_time_string();
+        if(timers[i].ended){
+            timers[i].ended = false;
+            timer_divs[i].style.borderColor = 'red';
+        }
+    }
+}, 200);
+
+function set_background_color(index, color){
+    timer_divs[index].style.background = color;
+};
---
+++ js/timer.js
@@ -0,0 +1,55 @@
+class Timer{
+    constructor(duration = 10){
+        this.start_date = Date.now();
+        this.previous_date = Date.now();
+        this.end_date = new Date(this.start_date);
+        this.end_date.setSeconds(this.end_date.getSeconds() + duration);
+        this.duration = duration;
+        this.running = false;
+        this.total_time = 0;
+        this.ended = false;
+        setInterval(()=>{
+            if(this.running)
+                this.update();
+        }, 500);
+    }
+
+    date_string(){
+        return this.milliseconds_to_time_string(this.running ? this.end_date - this.previous_date : this.duration * 1000);
+    }
+
+    total_time_string(){
+        return this.milliseconds_to_time_string(this.total_time);
+    }
+
+    milliseconds_to_time_string(milliseconds){
+        let seconds = Math.abs(Math.ceil(milliseconds / 1000));
+        let minutes = Math.floor(seconds / 60)
+        seconds %= 60;
+        return `${minutes.toLocaleString('en', {minimumIntegerDigits: 2})}:${seconds.toLocaleString('en', {minimumIntegerDigits: 2})}`;
+    }
+
+    update(){
+        this.total_time += Date.now() - this.previous_date;
+        this.previous_date = Date.now();
+        let diff = this.end_date - this.previous_date;
+        if(diff <= 0){
+            this.running = false;
+            this.ended = true;
+            return
+        }
+    }
+
+    start(){
+        this.start_date = Date.now();
+        this.previous_date = Date.now();
+        this.end_date = new Date(this.start_date);
+        this.end_date.setSeconds(this.end_date.getSeconds() + this.duration);
+        this.running = true;
+        this.ended = false;
+    }
+
+    stop(){
+        this.running = false;
+    }
+}
